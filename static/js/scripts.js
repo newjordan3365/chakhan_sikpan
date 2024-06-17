31f95bd95777fc0e996bbeaf35f372682e092abf
newjordan3365 <113762136+newjordan3365@users.noreply.github.com>
2024-06-17 11:10:01 +0900

    Add files via upload

---
+++ static/js/scripts.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // 추가적인 스크립트를 작성할 수 있습니다.
+});
+
+// Datepicker Initialization
+$('#datepicker').datepicker({
+    format: 'yyyy-mm-dd',
+    language: 'ko',
+    autoclose: true,
+    todayHighlight: true,
+    orientation: 'bottom'
+});
+
